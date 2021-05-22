ba47925bae19bd0db96d1fe6ae3cbaa44f9e96c4
Kelley Worthington <kelleymarne@gmail.com>
2021-05-21 22:18:24 -0600

    fixed timer interval to stop at 0 and switch to the page to enter in your inititals

--- main.js
+++ main.js
@@ -1,18 +1,54 @@
-const startButton = document.getElementById('start-btn')
-const questionContainerEl = document.getElementById('question-container')
-const questionEl = document.getElementById('question')
+let startButton = document.getElementById('start-btn')
+let questionContainerEl = document.getElementById('question-container')
+let questionEl = document.getElementById('question')
 const answerButtonsEl = document.getElementsByClassName('btn')
 let scoreEl = document.getElementById('score')
-let timeEl = document.getElementById('time')
+let endQuizEl = document.getElementById('end-quiz')
+let highScoreEl = document.getElementById('highscores')
+let endQuizBtn = document.getElementById('hs-btn')
 let score = 0;
 let counter = 0;
-let timerCounter = 60;
 
 
 const questions =
     [
         {
-            question: 'Question One!',
+            question: 'Javascript is the easiest language to learn',
+            0: 'Definitley Not!',
+            1: 'For Sure',
+            2: 'Is This Even English?',
+            3: 'I Could Do This In My Sleep',
+            answer: 'Is This Even English?'
+        },
+        {
+            question: 'Question two!',
+            0: 'answer 11**',
+            1: 'answer 12 test ',
+            2: 'answer 2',
+            3: 'answer 14',
+            answer: "answer 2"
+        },
+
+        {
+            question: 'Question two!',
+            0: 'answer 11**',
+            1: 'answer 12 test ',
+            2: 'answer 2',
+            3: 'answer 14',
+            answer: "answer 2"
+        },
+
+        {
+            question: 'Question two!',
+            0: 'answer 11**',
+            1: 'answer 12 test ',
+            2: 'answer 2',
+            3: 'answer 14',
+            answer: "answer 2"
+        },
+
+        {
+            question: 'Question two!',
             0: 'answer 11**',
             1: 'answer 12 test ',
             2: 'answer 2',
@@ -27,90 +63,85 @@ const questions =
             3: 'answer 14',
             answer: "answer 2"
         },
-        
         {
-            question: 'Question Three!',
-            answers: [
-                { a: 'answer 1' },
-                { b: 'answer 2', correct: true },
-                { c: 'answer 3' },
-                { d: 'answer 4' }
-            ]
+            question: 'Question two!',
+            0: 'answer 11**',
+            1: 'answer 12 test ',
+            2: 'answer 2',
+            3: 'answer 14',
+            answer: "answer 2"
         },
+        {
+            question: 'Question two!',
+            0: 'answer 11**',
+            1: 'answer 12 test ',
+            2: 'answer 2',
+            3: 'answer 14',
+            answer: "answer 2"
+        }];
 
-        {
-            question: 'Question Four!',
-            answers: [
-                { a: 'answer 1' },
-                { b: 'answer 2' },
-                { c: 'answer 3' },
-                { d: 'answer 4', correct: true }
-            ]
-        },
 
-        {
-            question: 'Question Five!',
-            answers: [
-                { a: 'answer 1' },
-                { b: 'answer 2' },
-                { c: 'answer 3', correct: true },
-                { d: 'answer 4' }
-            ]
-        },
+var timeLeft = 10;
 
-        {
-            question: 'Question Six!',
-            answers: [
-                { a: 'answer 1', correct: true },
-                { b: 'answer 2' },
-                { c: 'answer 3' },
-                { d: 'answer 4' }
-            ]
-        },
-    ]
-function timer() {
-let countDown = setInterval(() => {
-    
-    timerCounter--;
-    timeEl.textContent = timerCounter;
-}, 1000);
+var timerId = setInterval(countdown, 1000);
+let timeEl = document.getElementById('time')
+
+function countdown() {
+  if (timeLeft <= 0) {
+    clearInterval(timerId);
+    endQuiz();
+  } else {
+    timeEl.textContent = timeLeft;
+    timeLeft--;
+  }
 }
-
 
 function startGame() {
-    timer();
+    countdown();
     console.log('started')
     startButton.classList.add('hide');
-    questionContainerEl.classList.remove('hide')
+    questionContainerEl.classList.remove('hide');
     nextQuestion();
 }
+
+function endQuiz() {
+    console.log('done');
+    // stop timer 
+    questionContainerEl.classList.add('hide');
+    endQuizEl.classList.remove('hide');
+    endQuizBtn.classList.remove('hide');
+    // highscores page
+}
+
 
 function nextQuestion() {
     questionEl.textContent = questions[counter].question
     for (let i = 0; i < answerButtonsEl.length; i++) {
         answerButtonsEl[i].textContent = questions[counter][i];
-        console.log(answerButtonsEl)
 
         answerButtonsEl[i].addEventListener('click', function (e) {
             checkAnswer(e.target.textContent);
         })
     }
+    if (question === questions.length) {
+        endQuiz();
+    }
 }
 
 function checkAnswer(userAnswer) {
-    if(questions[counter].answer === userAnswer) {
-        score+=5;
+    if (questions[counter].answer === userAnswer) {
+        score += 5;
         scoreEl.textContent = score;
         console.log('correct')
     } else {
-        timerCounter-=5;
+        timeLeft -= 5;
         console.log('incorrect')
     }
 
     counter++;
+    console.log(counter);
     nextQuestion();
 }
-
 
 
 // answerButtonsEl[0].addEventListener('click', function(e) {
