f7154fd3c470e643c006aca1ec0e3bb12d97e0ec
Kelley Worthington <kelleymarne@gmail.com>
2021-05-23 19:13:30 -0600

    fixed append li items to document

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// GLOBAL VARIABLES ================================
+
 let startButton = document.getElementById('start-btn')
 let questionContainerEl = document.getElementById('question-container')
 let questionEl = document.getElementById('question')
@@ -6,96 +8,100 @@ let scoreEl = document.getElementById('score')
 let endQuizEl = document.getElementById('end-quiz')
 let highScoreEl = document.getElementById('highscores')
 let endQuizBtn = document.getElementById('hs-btn')
+let sumbitScoreEl = document.getElementById('submit-score');
+let savedScoresEl = document.getElementById('score-board')
 let score = 0;
 let counter = 0;
 
+// QUESTION ARRAY ===============
 
 const questions =
     [
         {
-            question: 'Javascript is the easiest language to learn',
-            0: 'Definitley Not!',
-            1: 'For Sure',
-            2: 'Is This Even English?',
-            3: 'I Could Do This In My Sleep',
-            answer: 'Is This Even English?'
+            question: 'Javascript is the ______ of a webpage',
+            0: 'Structure',
+            1: 'Style',
+            2: 'Behavior',
+            3: 'Retreival',
+            answer: 'Behavior'
         },
         {
-            question: 'Question two!',
-            0: 'answer 11**',
-            1: 'answer 12 test ',
-            2: 'answer 2',
-            3: 'answer 14',
-            answer: "answer 2"
-        },
-
-        {
-            question: 'Question two!',
-            0: 'answer 11**',
-            1: 'answer 12 test ',
-            2: 'answer 2',
-            3: 'answer 14',
-            answer: "answer 2"
+            question: 'Javascript files will always end with what type of file name?',
+            0: '.js',
+            1: '.html',
+            2: '.css',
+            3: '.MD',
+            answer: ".js"
         },
 
         {
-            question: 'Question two!',
-            0: 'answer 11**',
-            1: 'answer 12 test ',
-            2: 'answer 2',
-            3: 'answer 14',
-            answer: "answer 2"
+            question: 'Inside of which HTML element do you put your script tag?',
+            0: '<javascript>',
+            1: '<js>',
+            2: '<script>',
+            3: '<scripting>',
+            answer: '<script>'
         },
 
         {
-            question: 'Question two!',
-            0: 'answer 11**',
-            1: 'answer 12 test ',
-            2: 'answer 2',
-            3: 'answer 14',
-            answer: "answer 2"
+            question: 'How would you change the content of the p tag?  <p id="demo">This is a demo</p>',
+            0: '#demo.innerHTML = "Hello World";',
+            1: 'document.getElementByName("p").innerHTML = "Hello World!";',
+            2: 'document.getElement("p").innerHTML = "Hello World!";',
+            3: 'document.getElementById("demo").innerHTML = "Hello World!";',
+            answer: 'document.getElementById("demo").innerHTML = "Hello World!";'
+        },
+
+        {
+            question: 'Which syntax would show "Hello World!" in an alert box',
+            0: 'msg("Hello World");',
+            1: 'alerBox("Hello World!");',
+            2: 'alert("Hello World!");',
+            3: 'msgBox("Hello World");',
+            answer: 'alert("Hello World!");'
         },
         {
-            question: 'Question two!',
-            0: 'answer 11**',
-            1: 'answer 12 test ',
-            2: 'answer 2',
-            3: 'answer 14',
-            answer: "answer 2"
+            question: 'What is the correct syntax to create a function?',
+            0: 'function myFunction()',
+            1: 'function:myFunction()',
+            2: 'function = myFunction()',
+            3: 'var function = myFunction[]',
+            answer: 'function = myFunction()'
         },
         {
-            question: 'Question two!',
-            0: 'answer 11**',
-            1: 'answer 12 test ',
-            2: 'answer 2',
-            3: 'answer 14',
-            answer: "answer 2"
+            question: 'How would you call a function named "startQuiz"',
+            0: 'call startQuiz()',
+            1: 'startQuiz()',
+            2: 'call function startQuiz()',
+            3: 'startQuiz{}',
+            answer:'startQuiz()'
         },
         {
-            question: 'Question two!',
-            0: 'answer 11**',
-            1: 'answer 12 test ',
-            2: 'answer 2',
-            3: 'answer 14',
-            answer: "answer 2"
+            question: 'How do you correctly write an IF statement?',
+            0: 'if i=0 then',
+            1: 'if (i===0)',
+            2: 'if i=0',
+            3: 'if i === 0 then',
+            answer: 'if (i===0)'
         }];
 
-
-var timeLeft = 10;
+//  TIMER INFO ==================
+var timeLeft = 60;
 
 var timerId = setInterval(countdown, 1000);
 let timeEl = document.getElementById('time')
 
 function countdown() {
-  if (timeLeft <= 0) {
-    clearInterval(timerId);
-    endQuiz();
-  } else {
-    timeEl.textContent = timeLeft;
-    timeLeft--;
-  }
+    if (timeLeft <= 0) {
+        clearInterval(timerId);
+        endQuiz();
+    } else {
+        timeEl.textContent = timeLeft;
+        timeLeft--;
+    }
 }
 
+// START GAME FUNCTION
 function startGame() {
     countdown();
     console.log('started')
@@ -104,30 +110,30 @@ function startGame() {
     nextQuestion();
 }
 
+// END QUIZ FUNCTION
 function endQuiz() {
     console.log('done');
-    // stop timer 
     questionContainerEl.classList.add('hide');
     endQuizEl.classList.remove('hide');
     endQuizBtn.classList.remove('hide');
-    // highscores page
+    clearInterval(timerId);
 }
 
-
+//  CREATE NEXT QUESTION
 function nextQuestion() {
     questionEl.textContent = questions[counter].question
     for (let i = 0; i < answerButtonsEl.length; i++) {
         answerButtonsEl[i].textContent = questions[counter][i];
 
+
         answerButtonsEl[i].addEventListener('click', function (e) {
+            e.preventDefault();
             checkAnswer(e.target.textContent);
         })
     }
-    if (question === questions.length) {
-        endQuiz();
-    }
 }
 
+// CHECK FOR RIGHT OR WRONG ANSWERS
 function checkAnswer(userAnswer) {
     if (questions[counter].answer === userAnswer) {
         score += 5;
@@ -137,16 +143,58 @@ function checkAnswer(userAnswer) {
         timeLeft -= 5;
         console.log('incorrect')
     }
-
+    
     counter++;
-    console.log(counter);
-    nextQuestion();
+
+    if (counter === questions.length) {
+        endQuiz();
+    } else {
+        nextQuestion();
+    }
 }
 
+// ENTER INITIALS FOR HIGHSCORE
+function sumbitScore() {
+    let userScore = sumbitScoreEl.value.trim();
+    if (userScore === "") {
+        alert('Please type initials!')
+    } else {
+        let storedScores = JSON.parse(localStorage.getItem("highscores")) || [];
+        let userInfo =
+        {
+            initials: userScore,
+            score: score
+        };
+
+        storedScores.push(userInfo);
+        localStorage.setItem("highscores", JSON.stringify(storedScores));
+
+    }
+}
+
+// HIGH SCORE BOARD 
+function highScoreBoard() {
+    sumbitScore();
+
+    endQuizEl.classList.add('hide');
+    endQuizBtn.classList.add('hide');
+    highScoreEl.classList.remove('hide')
+
+    //   CREATE A NEW LI TO APPEND TO UL OF STORED SCORES
+    let storedScores = JSON.parse(localStorage.getItem("highscores")) || [];
+
+    storedScores.forEach(function (score) {
+        var scoreBoard = document.createElement('li');
+        scoreBoard.textContent = score.initials + ' : ' + score.score;
+        savedScoresEl.appendChild(scoreBoard);
+    })
+
+}
 
 // answerButtonsEl[0].addEventListener('click', function(e) {
 //     console.log(e.target.textContent);
 // })
-startButton.addEventListener('click', startGame)
 
-console.log(questions);
+startButton.addEventListener('click', startGame)
+endQuizBtn.addEventListener('click', highScoreBoard)
+
